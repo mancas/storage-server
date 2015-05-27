0fd489e045a12084833c045adc0640118dc33dd4
Manuel Casas <manuel.casasbarrado@gmail.com>
2015-05-27 12:52:01 +0200

    minor issue

--- js/app.js
+++ js/app.js
@@ -12,6 +12,155 @@
   var _deviceStorages = {};
   var _listeners = {};
 
+  function addEventTargetEvent(channel, request) {
+    var requestOp = request.remoteData.data;
+    var reqId = request.remoteData.id;
+    var deviceStorageId = requestOp.deviceStorageId;
+
+    function listenerTemplate(evt) {
+      channel.postMessage({
+        remotePortId: remotePortId,
+        data: {
+          id: reqId,
+          event: window.ServiceHelper.cloneObject(evt)
+        }
+      });
+    }
+
+    _listeners[reqId] = listenerTemplate;
+    _deviceStorages[deviceStorageId].addEventListener(requestOp.type,
+      _listeners[reqId], requestOp.useCapture);
+  }
+
+  function buildDOMCursorAnswer(operation, channel, request) {
+    var remotePortId = request.remotePortId;
+    // Params for the local operation:
+    var opData = request.remoteData.data.params || [];
+    var reqId = request.remoteData.id;
+    var deviceStorageId = request.remoteData.data.deviceStorageId;
+
+    // FIX-ME: Due to the way FakeDOMCursorRequest is implemented, we
+    // have to return all the fetched data on a single message
+    var cursor = _deviceStorages[deviceStorageId][operation](...opData);
+    var files = [];
+
+    cursor.onsuccess = () => {
+      if (!cursor.done) {
+        cursor.result && files.push(cursor.result);
+        cursor.continue();
+      } else {
+        // Send message
+        channel.postMessage({
+          remotePortId: remotePortId,
+          data: { id : reqId, result: files}}
+        );
+      }
+    };
+
+    cursor.onerror = () => {
+      channel.postMessage({
+        remotePortId: remotePortId,
+        data: {
+          id : reqId,
+          error: window.ServiceHelper.cloneObject(cursor.error)
+        }}
+      );
+    };
+  }
+
+  function buildDOMRequestAnswer(operation, channel, request) {
+    debug('Building call --> ' + JSON.stringify(request));
+    var remotePortId = request.remotePortId;
+    // Params for the local operation:
+    var opData = request.remoteData.data.params || [];
+    var reqId = request.remoteData.id;
+    var deviceStorageId = request.remoteData.data.deviceStorageId;
+    
+    _deviceStorages[deviceStorageId][operation](...opData).then(result => {
+      channel.postMessage({
+        remotePortId: remotePortId,
+        data: { id : reqId, result: result}}
+      );
+    }).catch(error => {
+      channel.postMessage({
+        remotePortId: remotePortId,
+        data: {
+          id : reqId,
+          error: window.ServiceHelper.cloneObject(error)
+        }}
+      );
+    });
+  }
+
+  function setHandler(eventType, channel, request) {
+    var remotePortId = request.remotePortId;
+    var reqId = request.remoteData.id;
+    var deviceStorageId = request.remoteData.data.deviceStorageId;
+
+    function observerTemplate(evt) {
+      channel.postMessage({
+        remotePortId: remotePortId,
+        data: {
+          id: reqId,
+          event: {
+            path: evt.path,
+            reason: evt.reason
+          }
+        }
+      });
+    }
+
+    _deviceStorages[deviceStorageId][eventType] = observerTemplate;
+  }
+
+  var _operations = {
+    getDeviceStorage: function(channel, request) {
+      var remotePortId = request.remotePortId;
+      var reqId = request.remoteData.id;
+      var opData = request.remoteData.data.params || [];
+      var requestOp = request.remoteData.data;
+
+      var deviceStorages = navigator.getDeviceStorages(...opData);
+      deviceStorages.forEach(ds => {
+        if (ds.storageName === requestOp.storageName) {
+          _deviceStorages[reqId] = ds;
+          return;
+        }
+      });
+      // Let's assume this works always...
+      channel.postMessage({remotePortId: remotePortId, data: {id: reqId}});
+    },
+
+    addEventListener: addEventTargetEvent.bind(this),
+
+    removeEventListener: function(channel, request) {
+      var requestOp = request.remoteData.data;
+      var deviceStorageId = requestOp.deviceStorageId;
+      _deviceStorages[deviceStorageId].
+        removeEventListener(_listeners[requestOp.listenerId]);
+    },
+
+    dispatchEvent: function(channel, request) {
+      var requestOp = request.remoteData.data;
+      var deviceStorageId = requestOp.deviceStorageId;
+      _deviceStorages[deviceStorageId].dispatchEvent(requestOp.event);
+    },
+
+    enumerate: buildDOMCursorAnswer.bind(this, 'enumerate'),
+
+    enumerateEditable: buildDOMCursorAnswer.bind(this, 'enumerateEditable'),
+
+    onchange: setHandler.bind(undefined, 'onchange')
+  };
+
+  ['add', 'addNamed',
+    'available', 'delete',
+    'freeSpace', 'get',
+    'getEditable', 'usedSpace',
+    'enumerate', 'enumerateEditable'].forEach(method => {
+      _operations[method] = buildDOMRequestAnswer.bind(undefined, method);
+  });
+
   var processSWRequest = function(channel, evt) {
     // We can get:
     // * methodName
@@ -20,110 +169,16 @@
     // * addEventListener
     // * removeEventListener
     // * dispatchEvent
-    // All the operations have a requestId, and the lock operations also include
-    // a deviceStorage id.
-    var remotePortId = evt.data.remotePortId;
     var request = evt.data.remoteData;
-    var requestOp = request.data;
+    var requestOp = request.data.operation;
 
-    function observerTemplate(evt) {
-      channel.postMessage({
-        remotePortId: remotePortId,
-        data: {
-          id: request.id,
-          data: {
-            path: evt.path,
-            reason: evt.reason
-          }
-        }
-      });
-    }
-
-    function listenerTemplate(evt) {
-      channel.postMessage({
-        remotePortId: remotePortId,
-        data: {
-          id: request.id,
-          data: {
-            event: evt
-          }
-        }
-      });
-    }
-
-    if (requestOp.operation === 'getDeviceStorage') {
-      var deviceStorages = navigator.getDeviceStorages(requestOp.params);
-      deviceStorages.forEach(ds => {
-        if (ds.storageName === requestOp.storageName) {
-          _deviceStorages[request.id] = ds;
-          return;
-        }
-      });
-      // Let's assume this works always...
-      channel.postMessage({remotePortId: remotePortId, data: {id: request.id}});
-    } else if (requestOp.operation === 'onchange') {
-      _deviceStorages[requestOp.deviceStorageId].onchange = observerTemplate;
-    } else if (requestOp.operation === 'addEventListener') {
-      _listeners[request.id] = listenerTemplate;
-      _deviceStorages[requestOp.deviceStorageId].
-        addEventListener(requestOp.type, _listeners[request.id], requestOp.useCapture);
-    } else if (requestOp.operation === 'removeEventListener') {
-      _deviceStorages[requestOp.deviceStorageId].
-        removeObserver(_listeners[request.id]);
-    } else if (requestOp.operation === 'dispatchEvent') {
-      _deviceStorages[requestOp.deviceStorageId].dispatchEvent(requestOp.event);
-    } else if (requestOp.operation === 'enumerate' ||
-      requestOp.operation === 'enumerateEditable') {
-        var cursor =
-          _deviceStorages[requestOp.deviceStorageId][requestOp.operation].
-          apply(_deviceStorages[requestOp.deviceStorageId], requestOp.params);
-        var files = [];
-
-        cursor.onsuccess = () => {
-          var file = cursor.result;
-          // 'cursor.done' flag should be activated when the last file is
-          // reached. However, it seems that the flag is only is enabled in 
-          // the next iteration so we've always got an undefined file
-          if (typeof file !== 'undefined') {
-            files.push(cursor.result);
-          }
-
-          if (!cursor.done) {
-            cursor.continue();
-          } else {
-            console.info(files);
-            // Send message
-            channel.postMessage({
-              remotePortId: remotePortId,
-              data: { id : request.id, result: files}}
-            );
-          }
-        };
-
-        cursor.onerror = () => {
-          channel.postMessage({
-            remotePortId: remotePortId,
-            data: { id : request.id, error: JSON.stringify(cursor.error)}}
-          );
-        };
+    debug('processSWRequest --> processing a msg:' +
+          (evt.data ? JSON.stringify(evt.data): 'msg without data'));
+    if (requestOp in _operations) {
+      _operations[requestOp] &&
+        _operations[requestOp](channel, evt.data);
     } else {
-      var method = 'call';
-      if (requestOp.params && typeof requestOp.params === 'object') {
-        method = 'apply';
-      }
-      _deviceStorages[requestOp.deviceStorageId][requestOp.operation]
-        [method](_deviceStorages[requestOp.deviceStorageId], requestOp.params).
-          then(result => {
-            channel.postMessage({
-              remotePortId: remotePortId,
-              data: { id : request.id, result: result}}
-            );
-      }).catch(error => {
-        channel.postMessage({
-          remotePortId: remotePortId,
-          data: { id : request.id, error: JSON.stringify(error)}}
-        );
-      });
+      console.error('DeviceStorage service unknown operation:' + requestOp);
     }
   };
 
